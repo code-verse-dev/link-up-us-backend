2c2095ce3cf8c620bc4ea1dc9508e4c3c33604f6
Ali Hussain <ali.hussain@salsoft.net>
2026-03-13 16:39:38 +0500

    removed tsc files

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
     apps: [
       {
         name: "link-up-us",       // Name of your app in PM2
-        script: "dist/index.js",      // Entry file of your Node.js app
+        script: "server.js",      // Entry file of your Node.js app
         instances: 1,             // or "max" for cluster mode
         autorestart: true,
         watch: false,
