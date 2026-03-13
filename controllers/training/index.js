2eb01cec6e2994f057b039497777bdbd17ea371b
Ali Hussain <ali.hussain@salsoft.net>
2026-03-13 21:22:33 +0500

    added req files

--- controllers/training/index.js
+++ controllers/training/index.js
@@ -1,6 +1,7 @@
 const TrainingVideo = require("../../models/TrainingVideo");
 const { ApiResponse } = require("../../helpers");
 const mongoose = require("mongoose");
+const { Readable } = require("stream");
 
 exports.list = async (req, res) => {
   try {
@@ -26,7 +27,48 @@ exports.play = async (req, res) => {
       return res.status(404).json(ApiResponse({}, "Video URL missing", false));
     }
 
-    return res.redirect(video.videoUrl);
+    const upstreamHeaders = {};
+    if (req.headers.range) {
+      upstreamHeaders.Range = req.headers.range;
+    }
+
+    const upstream = await fetch(video.videoUrl, {
+      method: "GET",
+      headers: upstreamHeaders,
+      redirect: "follow",
+    });
+
+    if (!upstream.ok && upstream.status !== 206) {
+      return res
+        .status(502)
+        .json(ApiResponse({}, `Upstream video failed (${upstream.status})`, false));
+    }
+
+    const passHeaders = [
+      "content-type",
+      "content-length",
+      "content-range",
+      "accept-ranges",
+      "cache-control",
+      "etag",
+      "last-modified",
+    ];
+    for (const header of passHeaders) {
+      const value = upstream.headers.get(header);
+      if (value) res.setHeader(header, value);
+    }
+    if (!res.getHeader("content-type")) {
+      res.setHeader("content-type", "video/mp4");
+    }
+
+    res.status(upstream.status === 206 ? 206 : 200);
+
+    if (!upstream.body) {
+      return res.status(502).json(ApiResponse({}, "Upstream video stream missing", false));
+    }
+
+    Readable.fromWeb(upstream.body).pipe(res);
+    return;
   } catch (err) {
     return res.status(500).json(ApiResponse({}, err.message, false));
   }
